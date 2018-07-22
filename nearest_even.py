d3397e27e5bd01810e39af70f758b301649dd61b
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-22 13:42:50 +0530

    Update and rename README.md to nearest_even.py

---
+++ nearest_even.py
@@ -0,0 +1,12 @@
+# nearest_even
+def main():
+    pass
+    n=int(input())
+    if (n%2==0):
+        print(n)
+    else:
+        k=(int(n)-1)
+        print(k)
+
+if __name__ == '__main__':
+    main()
